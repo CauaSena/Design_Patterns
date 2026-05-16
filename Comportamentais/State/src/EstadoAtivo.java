68ea047b963cd2d125ed9c88f6db0e1605373a45
Cauã Sena <cauafrcsena@gmail.com>
2026-05-16 14:18:35 -0300

    Corrigindo arquivos do Design Pattern State

--- Comportamentais/State/src/EstadoAtivo.java
+++ Comportamentais/State/src/EstadoAtivo.java
@@ -12,20 +12,18 @@ public class EstadoAtivo implements EstadoConta{
 	public void sacar(ContaBancaria conta, double valor) {
 		double novoSaldo = conta.getSaldo() - valor;
 		conta.setSaldo(novoSaldo);
-		verificarStatus(conta);
+		this.verificarStatus(conta);
 	}
 	
 	@Override
 	public void verificarStatus(ContaBancaria conta) {
 		if (conta.getSaldo() <= 0) {
 			conta.setStatus(new EstadoNegativo());
-			System.out.println("Saque realizado! Novo saldo: R$" + String.format("%.2f", conta.getSaldo()));
+			System.out.println("Saque realizado! Novo saldo: R$" + String.format("%.2f", conta.getSaldo()) + "\nAVISO - Contá está negativada!");
 			System.out.println("LEMBRETE: Limite do cheque especial: R$" + String.format("%.2f", conta.limiteChequeEspecial));
 		}else if (conta.getSaldo() < conta.limiteChequeEspecial) {
 			System.out.println("Operação Negada. Bloqueando conta por suspeita de fraude");
 			conta.setStatus(new EstadoBloqueado());
-		} else {
-			System.out.println("Saque realizado! Novo saldo: R$" + String.format("%.2f", conta.getSaldo()));
 		}
 	}
 }
--- Comportamentais/State/src/EstadoNegativo.java
+++ Comportamentais/State/src/EstadoNegativo.java
@@ -9,16 +9,16 @@ public class EstadoNegativo implements EstadoConta {
 			System.out.println("Saque realizado! Novo saldo: " + conta.getSaldo());
 			System.out.println("LEMBRETE: Limite do cheque especial: R$" + String.format("%.2f", conta.limiteChequeEspecial));
 		}else {
-			conta.setSaldo(novoSaldo);
-			System.out.println("Operação Negada.Bloqueando conta por suspeita de fraude");
-			verificarStatus(conta);
+				System.out.println("Operação Negada.Bloqueando conta por suspeita de fraude");
+				conta.setSaldo(-201);
+				this.verificarStatus(conta);
 		}
 	}
 
 	@Override
 	public void depositar(ContaBancaria conta, double valor) {
 		conta.setSaldo(conta.getSaldo() + (valor - conta.taxaDepositoNegativado));
-		verificarStatus(conta);
+		this.verificarStatus(conta);
 		System.out.println("Depósito realizado! Novo saldo: R$" + String.format("%.2f", conta.getSaldo()));
 		System.out.println("LEMBRETE - Caso esteja negativado, cada depósito terá uma taxa de: R$" + String.format("%.2f", conta.taxaDepositoNegativado));
 	}
